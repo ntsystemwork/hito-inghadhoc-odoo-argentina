9a70163343062e54c253916ee28435faf1416289
wsf <alejandro.sartorio@gmail.com>
2024-12-17 16:40:33 -0300

    [FIX] se vuelve a poner el store=true en campo computable

--- l10n_ar_ux/models/account_move.py
+++ l10n_ar_ux/models/account_move.py
@@ -13,7 +13,8 @@ class AccountMove(models.Model):
         string='Currency Rate (preview)',
         digits=(16, 6),
     )
-    l10n_ar_currency_rate = fields.Float(compute='_compute_l10n_ar_currency_rate')
+    
+    l10n_ar_currency_rate = fields.Float(compute='_compute_l10n_ar_currency_rate', store=True)
 
     @api.depends('reversed_entry_id')
     def _compute_l10n_ar_currency_rate(self):
